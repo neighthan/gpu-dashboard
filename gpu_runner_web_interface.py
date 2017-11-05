0286dbb1fcc1f51bc987375dd5ab7d8690368c23
Nathan Hunt <neighthan.hunt@gmail.com>
2017-11-04 23:13:18 -0400

    Moved job files to server.
    Instead of having one job file on each machine, the job files now reside on the server. dashboard only shows the jobs for the selected machine, and adding new commands will add them to the list for that machine.

--- gpu_runner_web_interface.py
+++ gpu_runner_web_interface.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-import logging
 from passlib.hash import sha256_crypt
 from functools import wraps
 import pickle
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-import signal
 import os
-import sys
-import pickle
 from getpass import getpass
-sys.path.append('../')
+import pymongo
 from gpu_utils import get_gpus
-from locking import cleanup, write_to_locked_file
 
 app = Flask(__name__)
+jobs_fname = '.gpu_jobs'
 
 
 def get_abs_path(relative_path: str) -> str:
@@ -70,8 +66,10 @@ def logout():
 @is_logged_in
 def dashboard():
     if request.method == 'POST':
-        write_to_locked_file(app.config['job_file'], str(request.get_json()['commands']), app.config['lock_dir'], app.config['lock_suffix'])
-        return ''  # I don't get this... have to return something, but it doesn't matter what? It won't render anything new
+        json = request.get_json()
+        with open(get_abs_path(f"{jobs_fname}_{json['machineName']}"), 'a+') as f:
+            f.write(json['commands'])
+        return ''
     else:
         return render_template('dashboard.html')
 
@@ -117,14 +115,16 @@ def gpu():
     return jsonify(gpus)
 
 
-@app.route('/data/jobs')
+@app.route('/data/jobs', methods=['POST'])  # because we need to send data (the machine name)
 @is_logged_in
 def jobs():
+    machine = request.get_json()['machine']
     try:
-        with open(app.config['job_file']) as f:
-            return jsonify(f.readlines())
+        with open(get_abs_path(f"{jobs_fname}_{machine['name']}")) as f:
+            jobs = f.readlines()
     except FileNotFoundError:
-        return '[]'
+        jobs = []
+    return jsonify(jobs)
 
 
 @app.route('/data/machines')
@@ -147,24 +147,16 @@ if __name__ == '__main__':
                                         "gpu which the job should use' &' will be added to the commands so that they run "
                                         "in the background; this shouldn't already be part of the command",
                             formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument('-f', '--job_file', help="Path to the file listing the jobs to run. The lock will be made in the "
-                                                 "same directory as the jobs file.", required=True)
     parser.add_argument('-st', '--sleep_time', help="How long, in seconds, to sleep between refreshing GPU info and the "
                                                     "jobs list. [default = 60]", type=float, default=60)
-    parser.add_argument('-ls', '--lock_suffix', help="Suffix that this script will use to tell that the lock on the file "
-                                                     "belongs to it. This shouldn't be used by any other script. "
-                                                     "[default = frontend]", default='frontend')
     parser.add_argument('-p', '--port', help="Which port to run on [default = 5000]", default=5000, type=int)
     parser.add_argument('-d', '--debug', help="Flag: whether to turn on debug mode", action='store_true')
 
     args = parser.parse_args()
     app.config.update(dict(
-        job_file    = args.job_file,
         sleep_time  = args.sleep_time,
-        lock_suffix = args.lock_suffix,
-        lock_dir    = os.path.dirname(args.job_file)),
         DEBUG       = args.debug
-    )
+    ))
 
     key_fname = get_abs_path('flask_key')
     passwords_fname = get_abs_path('passwords')
@@ -193,10 +185,6 @@ if __name__ == '__main__':
         chmod(passwords_fname, 0o600)
 
     ssh_password = getpass('Enter your SSH password: ')
-
-    # try to prevent this process from exiting without releasing the lock
-    for sig in [signal.SIGTERM, signal.SIGINT]:
-        signal.signal(sig, lambda signum, frame: cleanup(signum, frame, app.config['lock_dir'], args.lock_suffix))
 
     with open(key_fname, 'rb') as f:
         app.secret_key = f.read()
