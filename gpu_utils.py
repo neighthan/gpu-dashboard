93b932f43389670c7d96fbb4a92538da0411ee2a
Nathan Hunt <neighthan.hunt@gmail.com>
2017-11-14 20:10:00 -0500

    Moved tf_init to new tf_utils file.
    This is nice so that tensorflow is not a dependency of gpu_utils and thus of the gpu_runner.

--- gpu_utils.py
+++ gpu_utils.py
@@ -1,8 +1,6 @@
 from collections import namedtuple
 from subprocess import run, PIPE
-import os
-import tensorflow as tf
-from typing import Sequence, Optional, List
+from typing import Sequence, List
 
 GPU = namedtuple('GPU', ['num', 'mem_used', 'mem_free', 'util_used', 'util_free'])  # mem in MiB, util as % not used
 
@@ -76,25 +74,3 @@ def get_best_gpu(metric: str='util') -> int:
         best_gpu = max(gpus, key=lambda gpu: gpu.mem_free)
 
     return best_gpu.num
-
-
-def tf_init(device: Optional[int]=None, tf_logging_verbosity: str='1') -> tf.ConfigProto:
-    """
-    Runs common operations at start of TensorFlow:
-      - sets logging verbosity
-      - sets CUDA visible devices to `device` or, if `device` is '', to the GPU with the most free memory
-      - creates a TensorFlow config which allows for GPU memory growth and for soft placement
-    :param device: which GPU to use
-    :param tf_logging_verbosity: 0 for everything; 1 to remove info; 2 to remove warnings; 3 to remove errors
-    :returns: the aforementioned TensorFlow config
-    """
-
-    device = device if device is not None else get_best_gpu()
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(device)
-
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = tf_logging_verbosity
-
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.allow_soft_placement = True
-    return config
